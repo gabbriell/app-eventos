7957ceb5e97102c527a0b75b5524d5e3a79e17bf
Gabriel Martins <gabriel.javax@gmail.com>
2022-03-17 10:48:45 -0300

    Realizando buscas especificas no banco(listagem de convidados)

--- eventoapp/src/main/java/com/eventoapp/controller/EventoController.java
+++ eventoapp/src/main/java/com/eventoapp/controller/EventoController.java
@@ -1,6 +1,6 @@
 package com.eventoapp.controller;
 
-import javax.swing.JOptionPane;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,16 +9,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.eventoapp.model.Convidado;
 import com.eventoapp.model.Evento;
+import com.eventoapp.repository.ConvidadoRepository;
 import com.eventoapp.repository.EventoRepository;
 
 @Controller
 public class EventoController {
 	
-	
-	
 	@Autowired
 	private EventoRepository er;
+	
+	@Autowired
+	private ConvidadoRepository cr;
 	
 	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.GET)
 	public String form() {
@@ -41,14 +44,24 @@ public class EventoController {
 		return mv;
 	}
 	
-	@RequestMapping("/{codigo}")	
-	public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo) {
+	@RequestMapping(value="/{codigo}", method=RequestMethod.GET)	
+	public ModelAndView detalhesEventoPost(@PathVariable("codigo") long codigo) {
 		Evento evento = er.findByCodigo(codigo);
 		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
 		mv.addObject("evento", evento);
-		System.out.println("evento" + evento);
+		
+		Iterable<Convidado> convidados = cr.findByEvento(evento);
+		mv.addObject("convidados", convidados);
+		
 		return mv;
 	}
 	
+	@RequestMapping(value="/{codigo}", method=RequestMethod.POST)	
+	public String detalhesEvento(@PathVariable("codigo") long codigo, Convidado convidado) {
+		Evento evento = er.findByCodigo(codigo);
+		convidado.setEvento(evento);
+		cr.save(convidado);
+		return "redirect:/{codigo}";
+	}
 
 }
